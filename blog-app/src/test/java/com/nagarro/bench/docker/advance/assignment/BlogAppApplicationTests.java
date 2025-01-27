aea94c58419d1164e8fda9c6b3356ef60a3219c6
Saniya Singh <saniya.singh@nagarro.com>
2025-01-27 13:30:25 +0530

    add test file

--- blog-app/src/test/java/com/nagarro/bench/docker/advance/assignment/BlogAppApplicationTests.java
+++ blog-app/src/test/java/com/nagarro/bench/docker/advance/assignment/BlogAppApplicationTests.java
@@ -2,8 +2,10 @@ package com.nagarro.bench.docker.advance.assignment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BlogAppApplicationTests {
 
 	@Test
